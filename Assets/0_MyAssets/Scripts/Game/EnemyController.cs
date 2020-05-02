59dad5c2324fce8065530ede54dd3258f522bb11
家子智成 <>
2020-05-03 01:00:17 +0900

    箱を十曽

--- Assets/0_MyAssets/Scripts/Game/EnemyController.cs
+++ Assets/0_MyAssets/Scripts/Game/EnemyController.cs
@@ -21,8 +21,29 @@ public class EnemyController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collisionInfo)
     {
+        if (isDead) { return; }
+        HitBullet(collisionInfo);
+        HitHighSpeedObj(collisionInfo);
+    }
+
+    void HitHighSpeedObj(Collision2D collisionInfo)
+    {
+        var rb = collisionInfo.rigidbody;
+        if (rb == null) { return; }
+        //Debug.Log(rb.velocity.sqrMagnitude);
+        if (rb.velocity.sqrMagnitude < 5) { return; }
+        Killed();
+    }
+
+    void HitBullet(Collision2D collisionInfo)
+    {
         var bullet = collisionInfo.gameObject.GetComponent<BulletController>();
         if (bullet == null) { return; }
+        Killed();
+    }
+
+    void Killed()
+    {
         gameObject.layer = LayerMask.NameToLayer("Dead");
         isDead = true;
         deadPS.Play();
